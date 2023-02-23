88f2757c77d5b6f70e39d32d69808d74970292ed
isaacachenbach <isaac.achenbach1@gmail.com>
2023-02-23 11:08:40 -0600

    finished intro-to-testing-js exercise

--- code.js
+++ code.js
@@ -14,3 +14,15 @@ const sayHello = str => {
 
 const isFive = input => input === 5;
 
+const isEven = input => !isNaN(parseFloat(input)) ? input % 2 === 0 : false;
+
+const isVowel = input => {
+    let vowels = 'aeiou'
+    return typeof input === 'string' ? vowels.includes(input.toLowerCase()) : false;
+}
+
+const add = (input1, input2) => Number(input1) + Number(input2);
+
+console.log(add('banana', 'split'));
+
+
--- tests.js
+++ tests.js
@@ -74,3 +74,98 @@ describe('isFive', function() {
     });
 
 });
+
+describe('isEven', function() {
+    it('should be a defined function', function() {
+        expect(typeof isEven).toBe('function');
+    });
+    it('should return true when passed 6 as an input', function() {
+        expect(isEven(6)).toBe(true);
+    });
+    it('should return true when passed "8" as an input', function() {
+        expect(isEven("8")).toBe(true);
+    });
+    it('should return true when passed -4 as an input', function() {
+        expect(isEven(-4)).toBe(true);
+    });
+    it('should return false when passed 23 as an input', function() {
+        expect(isEven(23)).toBe(false);
+    });
+    it('should return false when passed "banana" as an input', function() {
+        expect(isEven('banana')).toBe(false);
+    });
+    it('should return false when passed Infinity as an input', function() {
+        expect(isEven(Infinity)).toBe(false);
+    });
+    it('should return false when passed false as an input', function() {
+        expect(isEven(false)).toBe(false);
+    });
+    it('should return false when passed null as an input', function() {
+        expect(isFive(null)).toBe(false);
+    });
+    it('should return false when passed undefined as an input', function() {
+        expect(isFive(undefined)).toBe(false);
+    });
+
+});
+
+describe('isVowel', function() {
+    it('should be a defined function', function() {
+        expect(typeof isVowel).toBe('function');
+    });
+    it('should return true when passed "a" as an input', function() {
+        expect(isVowel('a')).toBe(true);
+    });
+    it('should return true when passed "E" as an input', function() {
+        expect(isVowel("A")).toBe(true);
+    });
+    it('should return false when passed "g" as an input', function() {
+        expect(isVowel('g')).toBe(false);
+    });
+    it('should return false when passed 23 as an input', function() {
+        expect(isVowel(23)).toBe(false);
+    });
+    it('should return false when passed "banana" as an input', function() {
+        expect(isVowel('banana')).toBe(false);
+    });
+    it('should return false when passed false as an input', function() {
+        expect(isVowel(false)).toBe(false);
+    });
+    it('should return false when passed true as an input', function() {
+        expect(isVowel(true)).toBe(false);
+    });
+    it('should return false when passed as an open function', function() {
+        expect(isVowel()).toBe(false);
+    });
+});
+
+describe('add', function() {
+    it('should be a defined function', function() {
+        expect(typeof add).toBe('function');
+    });
+    it('should return true when passed -3, -9 as inputs', function() {
+        expect(add(-3, -9)).toBe(-12);
+    });
+    it('should return 11 when passed "5", 6 as inputs', function() {
+        expect(add("5", 6)).toBe(11);
+    });
+    it('should return 5 when passed 2, 3 as inputs', function() {
+        expect(add(2, 3)).toBe(5);
+    });
+    it('should return NaN when passed "banana", "split" as inputs', function() {
+        expect(Number.isNaN(add('banana', 'split'))).toBe(true);
+    });
+    it('should return NaN when passed 2, "apples" an inputs', function() {
+        expect(Number.isNaN(add(2, 'apples'))).toBe(true);
+    });
+    it('should return NaN when passed as an open function', function() {
+        expect(Number.isNaN(add())).toBe(true);
+    });
+});
+// add(2, 3) returns 5
+// add(-3, -9) returns -12
+// add("5", 6) returns 11
+// add("-4", "10") returns 6
+// add("banana", "split") returns NaN
+// add(2, "apples") returns NaN
+// add() returns NaN
